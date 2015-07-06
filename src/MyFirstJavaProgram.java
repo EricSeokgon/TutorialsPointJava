ab4e39f143259a478eb79d15ce887de7e0b4df33
hadeslee <leesk55@gmail.com>
2015-07-07 00:13:41 +0900

    Initial commit

---
+++ src/MyFirstJavaProgram.java
@@ -0,0 +1,13 @@
+/**
+ * Created by hadeslee on 2015-07-07.
+ */
+public class MyFirstJavaProgram {
+
+   /* This is my first java program.
+    * This will print 'Hello World' as the output
+    */
+
+    public static void main(String []args) {
+        System.out.println("Hello World"); // prints Hello World
+    }
+}
