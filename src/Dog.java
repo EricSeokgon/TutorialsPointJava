28b13a72ce4b8f0a8e45231f692d8d6b0132a9cd
hadesleee <leesk55@gmail.com>
2015-07-08 14:54:38 +0900

    클래스 변수 유형

---
+++ src/Dog.java
@@ -0,0 +1,23 @@
+/**
+ * Project: TutorialsPointJava
+ * FileName: asdasd
+ * Date: 2015-07-08
+ * Time: 오후 2:53
+ * Author: sklee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Dog{
+    String breed;
+    int age;
+    String color;
+
+    void barking(){
+    }
+
+    void hungry(){
+    }
+
+    void sleeping(){
+    }
+}
