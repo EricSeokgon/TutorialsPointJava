4244c4b09f4ab4b332212aec347f4f20e54e5da1
hadesleee <leesk55@gmail.com>
2015-07-08 14:55:28 +0900

    생성자 예제

---
+++ src/Puppy.java
@@ -0,0 +1,17 @@
+/**
+ * Project: TutorialsPointJava
+ * FileName: afdasd
+ * Date: 2015-07-08
+ * Time: 오후 2:54
+ * Author: sklee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Puppy{
+    public Puppy(){
+    }
+
+    public Puppy(String name){
+        // This constructor has one parameter, name.
+    }
+}
