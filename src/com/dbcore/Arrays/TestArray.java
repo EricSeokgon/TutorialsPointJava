2df1721db4b82aaca5302e8b62b5274f6562fc32
hadesleee <leesk55@gmail.com>
2015-07-09 16:15:55 +0900

    A method may also return an array. For example, the method shown below returns an array that is the reversal of another array:

--- src/com/dbcore/Arrays/TestArray.java
+++ src/com/dbcore/Arrays/TestArray.java
@@ -43,6 +43,7 @@ public class TestArray {
         }
 
         printArray(new int[]{3, 1, 2, 6, 4, 2});
+        reverse(new int[]{3, 1, 2, 6, 4, 2});
 
     }
     /*Passing Arrays to Methods*/
@@ -50,5 +51,16 @@ public class TestArray {
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
+    }
+
+    public static int[] reverse(int[] list) {
+        int[] result = new int[list.length];
+
+        for (int i = 0, j = result.length - 1; i < list.length; i++, j--) {
+            result[j] = list[i];
+
+        }
+        return result;
+
     }
 }
