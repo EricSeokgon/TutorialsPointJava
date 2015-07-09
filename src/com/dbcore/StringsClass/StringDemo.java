e5bc4ec6111cd8dbea6c5e1b9c19dd6ea7ea37aa
hadesleee <leesk55@gmail.com>
2015-07-09 11:27:33 +0900

    문자열 리터럴을 만날 때마다, 컴파일러는이 경우, "안녕하세요 세계에서 값을 String 객체를 만듭니다! '.
    다른 개체와 마찬가지로 새로운 키워드와 생성자를 사용하여 String 객체를 생성 할 수 있습니다. String 클래스는 당신이 그런 문자의 배열과 같은 다른 소스를 사용하여 문자열의 초기 값을 제공 할 수 있도록 열한 생성자가 있습니다.

---
+++ src/com/dbcore/StringsClass/StringDemo.java
@@ -0,0 +1,19 @@
+package com.dbcore.StringsClass;
+
+/**
+ * Project: TutorialsPointJava
+ * FileName: asdas
+ * Date: 2015-07-09
+ * Time: 오전 11:25
+ * Author: sklee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class StringDemo{
+
+    public static void main(String args[]){
+        char[] helloArray = { 'h', 'e', 'l', 'l', 'o', '.'};
+        String helloString = new String(helloArray);
+        System.out.println( helloString );
+    }
+}
