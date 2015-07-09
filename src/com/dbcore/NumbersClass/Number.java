35296d89dd6beda2af85708bedef7d27871ed84a
hadesleee <leesk55@gmail.com>
2015-07-09 11:15:12 +0900

    Here is an example of boxing and unboxing:

---
+++ src/com/dbcore/NumbersClass/Number.java
@@ -0,0 +1,20 @@
+package com.dbcore.NumbersClass;
+
+/**
+ * Project: TutorialsPointJava
+ * FileName: test
+ * Date: 2015-07-09
+ * Time: 오전 11:11
+ * Author: sklee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Number {
+
+    public static void main(String args[]) {
+        Integer x = 5; // boxes int to an Integer object
+        x = x + 10;   // unboxes the Integer to a int
+
+        System.out.println(x);
+    }
+}
