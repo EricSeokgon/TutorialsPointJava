5cfaaebecc6a8992881c26dc170f6f4d549f9f8b
hadesleee <leesk55@gmail.com>
2015-07-09 15:39:42 +0900

    생성, 배열을 초기화하고 처리하는 방법을 보여주는 완벽한 예입니다

---
+++ src/com/dbcore/Arrays/TestArray.java
@@ -0,0 +1,34 @@
+package com.dbcore.Arrays;
+
+/**
+ * Project: TutorialsPointJava
+ * FileName: asdf
+ * Date: 2015-07-09
+ * Time: 오후 2:57
+ * Author: sklee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestArray {
+
+    public static void main(String[] args) {
+        double[] myList = {1.9, 2.9, 3.4, 3.5};
+
+        // Print all the array elements
+        for (int i = 0; i < myList.length; i++) {
+            System.out.println(myList[i] + " ");
+        }
+        // Summing all elements
+        double total = 0;
+        for (int i = 0; i < myList.length; i++) {
+            total += myList[i];
+        }
+        System.out.println("Total is " + total);
+        // Finding the largest element
+        double max = myList[0];
+        for (int i = 1; i < myList.length; i++) {
+            if (myList[i] > max) max = myList[i];
+        }
+        System.out.println("Max is " + max);
+    }
+}
