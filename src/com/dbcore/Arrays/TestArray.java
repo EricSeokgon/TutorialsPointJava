337c9d5ec0468aa054db4fcf06a0c08719362521
hadesleee <leesk55@gmail.com>
2015-07-09 15:45:57 +0900

    생성, 배열을 초기화하고 처리하는 방법을 보여주는 완벽한 예입니다

--- src/com/dbcore/Arrays/TestArray.java
+++ src/com/dbcore/Arrays/TestArray.java
@@ -13,6 +13,7 @@ public class TestArray {
 
     public static void main(String[] args) {
         double[] myList = {1.9, 2.9, 3.4, 3.5};
+        String num ="";
 
         // Print all the array elements
         for (int i = 0; i < myList.length; i++) {
@@ -30,5 +31,10 @@ public class TestArray {
             if (myList[i] > max) max = myList[i];
         }
         System.out.println("Max is " + max);
+
+        for (double x : myList) {
+            num += (num.equals("")) ? x : ", " + x + "";
+        }
+        System.out.println("num is " + num);
     }
 }
