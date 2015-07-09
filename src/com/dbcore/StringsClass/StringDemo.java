cf4d8c6b5ecf564af81338fafd8822622fd35968
hadesleee <leesk55@gmail.com>
2015-07-09 14:50:37 +0900

    문자열 연결

--- src/com/dbcore/StringsClass/StringDemo.java
+++ src/com/dbcore/StringsClass/StringDemo.java
@@ -12,18 +12,20 @@ package com.dbcore.StringsClass;
 public class StringDemo {
 
     public static void main(String args[]) {
+        /*문자열 만들기*/
         char[] helloArray = {'h', 'e', 'l', 'l', 'o', '.'};
         String helloString = new String(helloArray);
         System.out.println(helloString);
-
+        /*문자열 길이*/
         String palindrome = "Dot saw I was Tod";
         int len = palindrome.length();
         System.out.println( "String Length is : " + len );
-
+        /*문자열 연결*/
         String string1 = "saw I was ";
         System.out.println("Dot " + string1 + "Tod");
 
         String string2 = "Tedra ";
         System.out.println(string1.concat(string2));
+
     }
 }
