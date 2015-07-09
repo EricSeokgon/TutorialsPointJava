7aea87bc9770ce9f0df7edf3643c997884a3d20f
hadesleee <leesk55@gmail.com>
2015-07-09 11:31:01 +0900

    String Length

--- src/com/dbcore/StringsClass/StringDemo.java
+++ src/com/dbcore/StringsClass/StringDemo.java
@@ -9,11 +9,15 @@ package com.dbcore.StringsClass;
  * Note:
  * To change this template use File | Settings | File Templates.
  */
-public class StringDemo{
+public class StringDemo {
 
-    public static void main(String args[]){
-        char[] helloArray = { 'h', 'e', 'l', 'l', 'o', '.'};
+    public static void main(String args[]) {
+        char[] helloArray = {'h', 'e', 'l', 'l', 'o', '.'};
         String helloString = new String(helloArray);
-        System.out.println( helloString );
+        System.out.println(helloString);
+
+        String palindrome = "Dot saw I was Tod";
+        int len = palindrome.length();
+        System.out.println( "String Length is : " + len );
     }
 }
