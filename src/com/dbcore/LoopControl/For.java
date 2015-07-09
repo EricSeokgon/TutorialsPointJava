4f0ec57f38a72a347a4cdab90d6c66da341c0f16
hadesleee <leesk55@gmail.com>
2015-07-09 10:43:01 +0900

    for loop

---
+++ src/com/dbcore/LoopControl/For.java
@@ -0,0 +1,20 @@
+package com.dbcore.LoopControl;
+
+/**
+ * Project: TutorialsPointJava
+ * FileName: For
+ * Date: 2015-07-09
+ * Time: 오전 10:41
+ * Author: sklee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class For {
+    public static void main(String args[]) {
+
+        for(int x = 10; x < 20; x = x+1) {
+            System.out.print("value of x : " + x );
+            System.out.print("\n");
+        }
+    }
+}
