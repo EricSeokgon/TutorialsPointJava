5f740443efd90500b379dbd3af5bf2cc77c6caba
hadesleee <leesk55@gmail.com>
2015-07-09 10:39:54 +0900

    표현이 테스트 결과가 false 경우, 루프 본문은 건너 뛰고 while 루프 후 첫 번째 문이 실행됩니다.

---
+++ src/com/dbcore/LoopControl/While.java
@@ -0,0 +1,22 @@
+package com.dbcore.LoopControl;
+
+/**
+ * Project: TutorialsPointJava
+ * FileName: While
+ * Date: 2015-07-09
+ * Time: 오전 10:38
+ * Author: sklee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class While {
+    public static void main(String[] args) {
+        int x = 10;
+
+        while( x < 20 ) {
+            System.out.print("value of x : " + x );
+            x++;
+            System.out.print("\n");
+        }
+    }
+}
