c7e0655a3bc7cf00d4c6f0a7595c6ee2ff74c604
hadesleee <leesk55@gmail.com>
2015-07-09 11:03:03 +0900

    if 문은 옵션 다음에 할 수 밖에 부울 표현식이 거짓 인 경우 실행 문.

---
+++ src/com/dbcore/DecisionMaking/IfElse.java
@@ -0,0 +1,22 @@
+package com.dbcore.DecisionMaking;
+
+/**
+ * Project: TutorialsPointJava
+ * FileName: IfElse
+ * Date: 2015-07-09
+ * Time: 오전 11:02
+ * Author: sklee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class IfElse {
+    public static void main(String args[]) {
+        int x = 30;
+
+        if (x < 20) {
+            System.out.print("This is if statement");
+        } else {
+            System.out.print("This is else statement");
+        }
+    }
+}
