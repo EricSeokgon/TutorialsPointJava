30ad9168095768cda89f76c4c891f01853452c04
hadesleee <leesk55@gmail.com>
2015-07-09 16:10:38 +0900

    방법으로 원시 형 값을 전달할 수있는 것처럼, 당신은 또한 방법에 배열을 전달할 수 있습니다. 예를 들어, 다음의 방법은 int 배열 요소를 표시

--- src/com/dbcore/Arrays/TestArray.java
+++ src/com/dbcore/Arrays/TestArray.java
@@ -41,5 +41,14 @@ public class TestArray {
         for (double element: myList) {
             System.out.println(element);
         }
+
+        printArray(new int[]{3, 1, 2, 6, 4, 2});
+
+    }
+    /*Passing Arrays to Methods*/
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
     }
 }
