bb83974681e4ece4d796b4f52d9dfccfab1dbad6
hadesleee <leesk55@gmail.com>
2015-07-09 10:04:27 +0900

    리터럴은 임의의 원시 형의 변수에 할당 할 수 있습니다

---
+++ src/BasicDataType.java
@@ -0,0 +1,29 @@
+/**
+ * Project: TutorialsPointJava
+ * FileName: BasicDataType
+ * Date: 2015-07-09
+ * Time: 오전 9:58
+ * Author: sklee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class BasicDataType {
+    public static void main(String[] args) {
+
+        /* Java Literals ex) */
+
+        byte a = 68;
+        char a1 ='A';
+
+        int decimal = 100;
+        int octal = 0144;
+        int hexa = 0x64;
+
+        char a2 ='\u0001';
+        String a3 ="\u0001";
+
+
+
+
+    }
+}
