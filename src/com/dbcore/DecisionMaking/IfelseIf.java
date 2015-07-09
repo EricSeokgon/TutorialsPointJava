c36ae1b990f44698dadef7efc3662ebf47ba135c
hadesleee <leesk55@gmail.com>
2015-07-09 11:04:32 +0900

    if 문은 옵션으로 올 수 있습니다 ... 다른 경우 다른 ... 경우 단일 사용하여 다양한 조건을 테스트하기 위해 매우 유용 문, 다른 if 문.
    경우 다른, 경우에 사용하는 경우, 다른 문은 명심해야 할 몇 가지 포인트가있다.
    
    할 수있는 경우 0 또는 1 다른 사람의 그것이 어떤 사람의 경우 후 와야합니다.
    
    경우의 다른 많은 사람들에게 영을 가질 수 있습니다 그리고 그들은 다른 사람 앞에 와야합니다.
    
    다른 성공하면 일단 경우 나머지 다른 하나도 없습니다 또는 다른 사람이 테스트됩니다.

---
+++ src/com/dbcore/DecisionMaking/IfelseIf.java
@@ -0,0 +1,26 @@
+package com.dbcore.DecisionMaking;
+
+/**
+ * Project: TutorialsPointJava
+ * FileName: IfelseIf
+ * Date: 2015-07-09
+ * Time: 오전 11:03
+ * Author: sklee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class IfelseIf {
+    public static void main(String args[]) {
+        int x = 30;
+
+        if (x == 10) {
+            System.out.print("Value of X is 10");
+        } else if (x == 20) {
+            System.out.print("Value of X is 20");
+        } else if (x == 30) {
+            System.out.print("Value of X is 30");
+        } else {
+            System.out.print("This is else statement");
+        }
+    }
+}
