731d541d030c3f88916cd0314010abc78ee30e58
hadesleee <leesk55@gmail.com>
2015-07-09 11:06:46 +0900

    중첩 If

---
+++ src/com/dbcore/DecisionMaking/NnestedIf.java
@@ -0,0 +1,23 @@
+package com.dbcore.DecisionMaking;
+
+/**
+ * Project: TutorialsPointJava
+ * FileName: NnestedIf
+ * Date: 2015-07-09
+ * Time: 오전 11:06
+ * Author: sklee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class NnestedIf {
+    public static void main(String args[]) {
+        int x = 30;
+        int y = 10;
+
+        if (x == 30) {
+            if (y == 10) {
+                System.out.print("X = 30 and Y = 10");
+            }
+        }
+    }
+}
