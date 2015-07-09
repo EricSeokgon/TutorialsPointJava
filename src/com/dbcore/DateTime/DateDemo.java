1798f446966b911bd626d943d50e80a6fe7b2803
hadesleee <leesk55@gmail.com>
2015-07-09 16:20:19 +0900

    현재 날짜 및 시간 가져 오기

---
+++ src/com/dbcore/DateTime/DateDemo.java
@@ -0,0 +1,22 @@
+package com.dbcore.DateTime;
+
+/**
+ * Project: TutorialsPointJava
+ * FileName: asdasd
+ * Date: 2015-07-09
+ * Time: 오후 4:17
+ * Author: sklee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+import java.util.Date;
+
+public class DateDemo {
+    public static void main(String args[]) {
+        // Instantiate a Date object
+        Date date = new Date();
+
+        // display time and date using toString()
+        System.out.println(date.toString());
+    }
+}
