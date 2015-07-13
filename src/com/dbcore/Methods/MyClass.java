f3c7164b23f9d5abc302468f462d9c8ce05fa0fa
hadesleee <leesk55@gmail.com>
2015-07-13 10:31:45 +0900

    생성자를 사용하는 예제

---
+++ src/com/dbcore/Methods/MyClass.java
@@ -0,0 +1,19 @@
+package com.dbcore.Methods;
+
+/**
+ * Project: TutorialsPointJava
+ * FileName: MyClass
+ * Date: 2015-07-13
+ * Time: 오전 10:31
+ * Author: sklee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class MyClass {
+    int x;
+
+    MyClass() {
+        x = 10;
+    }
+
+}
