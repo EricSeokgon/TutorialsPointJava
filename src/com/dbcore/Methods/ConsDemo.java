39a47f22c776a5058e490d693f2c56f4098972b8
hadesleee <leesk55@gmail.com>
2015-07-13 10:33:45 +0900

    객체를 초기화하는 생성자를 호출하는 예

---
+++ src/com/dbcore/Methods/ConsDemo.java
@@ -0,0 +1,19 @@
+package com.dbcore.Methods;
+
+/**
+ * Project: TutorialsPointJava
+ * FileName: ConsDemo
+ * Date: 2015-07-13
+ * Time: 오전 10:32
+ * Author: sklee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConsDemo {
+    public static void main(String[] args) {
+        MyClass t1 = new MyClass();
+        MyClass t2 = new MyClass();
+
+        System.out.println(t1.x + " " + t2.x);
+    }
+}
