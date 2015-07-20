027a4dc48c264be2d37fb0eb7e9cc3160f188fcf
hadesleee <leesk55@gmail.com>
2015-07-20 09:23:39 +0900

    우리가 캡슐화를 묘사하는 예를 살펴 보자

---
+++ src/com/dbcore/Interfaces/NameOfInterface.java
@@ -0,0 +1,20 @@
+package com.dbcore.Interfaces;
+
+/**
+ * Project: TutorialsPointJava
+ * FileName: NameOf
+ * Date: 2015-07-20
+ * Time: 오전 9:22
+ * Author: sklee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+/* File name : NameOfInterface.java */
+import java.lang.*;
+//Any number of import statements
+
+public interface NameOfInterface
+{
+    //Any number of final, static fields
+    //Any number of abstract method declarations\
+}
