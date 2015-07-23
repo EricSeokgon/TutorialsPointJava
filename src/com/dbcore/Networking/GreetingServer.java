8782262b75f18f9341eca8b2a059bbd916e42f15
hadesleee <leesk55@gmail.com>
2015-07-23 10:52:05 +0900

    소켓 서버 예제

---
+++ src/com/dbcore/Networking/GreetingServer.java
@@ -0,0 +1,69 @@
+package com.dbcore.Networking;
+
+/**
+ * Project: TutorialsPointJava
+ * FileName: adsf
+ * Date: 2015-07-23
+ * Time: 오전 10:51
+ * Author: sklee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+// File Name GreetingServer.java
+
+import java.net.*;
+import java.io.*;
+
+public class GreetingServer extends Thread
+{
+    private ServerSocket serverSocket;
+
+    public GreetingServer(int port) throws IOException
+    {
+        serverSocket = new ServerSocket(port);
+        serverSocket.setSoTimeout(10000);
+    }
+
+    public void run()
+    {
+        while(true)
+        {
+            try
+            {
+                System.out.println("Waiting for client on port " +
+                        serverSocket.getLocalPort() + "...");
+                Socket server = serverSocket.accept();
+                System.out.println("Just connected to "
+                        + server.getRemoteSocketAddress());
+                DataInputStream in =
+                        new DataInputStream(server.getInputStream());
+                System.out.println(in.readUTF());
+                DataOutputStream out =
+                        new DataOutputStream(server.getOutputStream());
+                out.writeUTF("Thank you for connecting to "
+                        + server.getLocalSocketAddress() + "\nGoodbye!");
+                server.close();
+            }catch(SocketTimeoutException s)
+            {
+                System.out.println("Socket timed out!");
+                break;
+            }catch(IOException e)
+            {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+    public static void main(String [] args)
+    {
+        int port = Integer.parseInt(args[0]);
+        try
+        {
+            Thread t = new GreetingServer(port);
+            t.start();
+        }catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
