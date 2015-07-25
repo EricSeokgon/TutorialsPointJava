c23026737138dcdc624030cfaaad3e1963ecc8fa
hadeslee <leesk55@gmail.com>
2015-07-26 00:22:03 +0900

    애플릿 테스트

---
+++ src/com/dbcore/ApplecBasics/HelloWorldApplet.java
@@ -0,0 +1,21 @@
+package com.dbcore.ApplecBasics;
+
+/**
+ * Project: TutorialsPointJava
+ * FileName: sdfsdf
+ * Date: 2015-07-26
+ * Time: 오전 12:20
+ * Author: hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+import java.applet.*;
+import java.awt.*;
+
+public class HelloWorldApplet extends Applet
+{
+    public void paint (Graphics g)
+    {
+        g.drawString ("Hello World", 25, 50);
+    }
+}
