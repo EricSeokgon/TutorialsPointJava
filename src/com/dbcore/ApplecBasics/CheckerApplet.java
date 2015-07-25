9c62b3ac3123d0d889505d34be20be3b48b2df44
hadeslee <leesk55@gmail.com>
2015-07-26 00:23:37 +0900

    애플릿 매개 변수를 얻기

---
+++ src/com/dbcore/ApplecBasics/CheckerApplet.java
@@ -0,0 +1,21 @@
+package com.dbcore.ApplecBasics;
+
+/**
+ * Project: TutorialsPointJava
+ * FileName: ㄴㅇㄹ
+ * Date: 2015-07-26
+ * Time: 오전 12:22
+ * Author: hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+import java.applet.*;
+import java.awt.*;
+public class CheckerApplet extends Applet
+{
+    int squareSize = 50;// initialized to default size
+    public void init () {}
+    private void parseSquareSize (String param) {}
+    private Color parseColor (String param) {}
+    public void paint (Graphics g) {}
+}
