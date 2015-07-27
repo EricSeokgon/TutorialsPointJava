7008da8be4b4636608620ff0f39f953910953317
hadesleee <leesk55@gmail.com>
2015-07-27 09:45:53 +0900

    자바 주석 예제

---
+++ src/com/dbcore/DocumentationComments/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.dbcore.DocumentationComments;
+
+/**
+ * Project: TutorialsPointJava
+ * FileName: sfs
+ * Date: 2015-07-27
+ * Time: 오전 9:44
+ * Author: sklee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        /* Prints Hello, World! on standard output. */
+        System.out.println("Hello World!");
+    }
+}
